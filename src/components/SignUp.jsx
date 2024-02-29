4f944462c27ea3ba1996280e55e922374026f49d
ZarifMustafa <118160736+ZarifMustafa@users.noreply.github.com>
2024-02-29 17:29:01 +0600

    SignUp added

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,27 @@
+import './SignUp.css';
+
+function SignUp() {
+  return (
+    <div className="SignUp">
+      <header>
+        
+      </header>
+      <body>
+        <div className="top-frame">
+          <div className="top-writing">
+            Top Frame
+          </div>
+        </div>
+        <div id="nav-bar">
+            <div id="nav-bar-frame-1">
+              <div id="logo">
+                TECHSTATION
+              </div>
+            </div>
+          </div>
+      </body>
+    </div>
+  );
+}
+
+export default SignUp;
