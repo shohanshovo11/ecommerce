602a84ef96494c9e4ad86623b30dde2252c35101
ZarifMustafa <118160736+ZarifMustafa@users.noreply.github.com>
2024-03-06 15:00:03 +0600

    Sign up creating account in database

--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -4,7 +4,34 @@ import sidebar_image from "./../../assets/img/side_image/login.svg";
 import Offer from "../Offer";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import Axios from "../../api/api"
+import { Link } from "react-router-dom";
 function Signup() {
+  const handleSubmit =async(e)=>{
+    const name=e.target.name.value;
+    const email=e.target.email.value;
+    const password=e.target.password.value;
+    e.preventDefault(); // Prevent default form submission
+    console.log("helo");
+    try {
+      // Make POST request using Axios
+      const response = await Axios.post("person/", {
+        name: name,
+        email: email,
+        password: password,
+        verified: true
+      });
+
+      // Handle response accordingly
+      console.log(response,"zarif"); // Log response data or do something with it
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        console.error("Error:", error.response.data.message); // Log any errors
+      } else {
+        console.error("An unexpected error occurred:", error);
+      }
+    }
+  }
   return (
     <>
       <Offer />
@@ -14,26 +41,30 @@ function Signup() {
           <div id="img">
             <img id="aa" src={sidebar_image}></img>
           </div>
-          <div id="login-field">
+          <form id="login-field" onSubmit={(e)=>handleSubmit(e)}>
             <div id="frame-1">
               <div id="create">Create an account</div>
               <div id="enter">Enter your details below</div>
             </div>
             <br />
             <div id="frame-2">
-              <input type="text" placeholder="Name" id="name"></input>
+              <input type="text" placeholder="Name" id="name" name="name"></input>
               <br />
               <input
                 type="email"
                 placeholder="Email or Phone Number"
                 id="mail"
+                name="email"
               ></input>
               <br />
-              <input type="password" placeholder="Password" id="pass"></input>
+              <input type="password" placeholder="Password" id="pass" name="password"></input>
               <br />
             </div>
             <div id="frame-3">
-              <button id="create-btn">Create Account</button>
+            <Link to="/">
+              <button id="create-btn" type="submit">
+                Create Account</button>
+                </Link>
               <div id="frame-4">
                 {/* <img src='./Icon-Google.png' alt="Google Icon"></img> */}
 
@@ -54,7 +85,7 @@ function Signup() {
                 </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
       <Footer />
