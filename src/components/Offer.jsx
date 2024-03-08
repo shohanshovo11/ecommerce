5cc2bf307290fb4fe6ad7ad37093fc5b65db1fe6
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-03-08 15:12:15 +0600

    offer page changed

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -5,7 +5,7 @@ function Offer() {
   return (
     <header className="bg-black hidden text-white font-extralight py-3 md:flex md:justify-center md:items-center gap-40">
       <div>
-        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+        Summer Sale For Electronic Device And Free Express Delivery - OFF 50%!
         <span className="underline pl-4 font-bold cursor-pointer">ShopNow</span>
       </div>
       <div className="">
